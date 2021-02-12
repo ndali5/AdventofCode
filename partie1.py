b150a06d6d7d2b9e6320a28b3b4752c7704ecf57
ndali5 <jeancgn@msn.com>
2021-02-12 09:39:42 +0100

    partie1

---
+++ partie1.py
@@ -0,0 +1,12 @@
+if __name__ == '__partie1__':
+    fichier = open('input1', 'r')
+    content = fichier.read()
+
+    montableau = [int(i) for i in content.split('\n')]
+    for i in montableau:
+        for j in montableau:
+            for h in montableau:
+                x = i + j + h
+                if x == 2020 :
+                    print (i, '+',j, '+',h ,'=', 2020)
+                    print(i*j*h)
